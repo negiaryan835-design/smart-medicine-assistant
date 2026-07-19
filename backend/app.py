b05da12cbbe4f00f94d5941b9c21073612f46c82
Aryan Negi <negiaryan835@gmail.com>
2026-07-19 13:23:06 +0530

    Added dashboard API endpoint

--- backend/app.py
+++ backend/app.py
@@ -7,3 +7,10 @@ def home():
     return {
         "message": "Smart Medicine Assistant Backend Running"
     }
+@app.get("/dashboard")
+def get_dashboard():
+    return {
+        "totalMedicines": 2,
+        "activeReminders": 3,
+        "medicinesScanned": 5
+    }
